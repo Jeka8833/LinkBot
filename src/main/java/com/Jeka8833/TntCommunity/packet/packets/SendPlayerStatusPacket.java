258d6cb1b223edf6fafe11e327fa40bbe022624a
Jeka8833 <eadercach@gmail.com>
2021-11-28 15:01:39 +0200

    Update

--- src/main/java/com/Jeka8833/TntCommunity/packet/packets/SendPlayerStatusPacket.java
+++ src/main/java/com/Jeka8833/TntCommunity/packet/packets/SendPlayerStatusPacket.java
@@ -4,6 +4,7 @@ import com.Jeka8833.TntCommunity.TNTUser;
 import com.Jeka8833.TntCommunity.packet.Packet;
 import com.Jeka8833.TntCommunity.packet.PacketInputStream;
 import com.Jeka8833.TntCommunity.packet.PacketOutputStream;
+import com.Jeka8833.TntCommunity.util.Util;
 import org.java_websocket.WebSocket;
 
 import java.io.IOException;
@@ -24,7 +25,7 @@ public record SendPlayerStatusPacket(List<TNTUser> users, boolean isAdmin) imple
                     case TNTUser.STATUS_AFK -> 2;
                     default -> 0;
                 });
-                stream.writeLong(isAdmin ? user.activeModules : user.activeModules);
+                stream.writeLong(isAdmin ? user.activeModules : (user.activeModules & Util.DJFix));
                 stream.writeUTF(user.version);
                 stream.writeUTF((isAdmin || user.status > 1) ? user.gameInfo : "");
             }
